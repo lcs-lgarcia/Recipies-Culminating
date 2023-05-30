c07e49f4f6a96e150b60e2ed58b4d6af1a7cf718
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-05-29 23:50:39 -0400

    Initial Commit

---
+++ Recipies Culminating/Recipies_CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipies_CulminatingApp.swift
+//  Recipies Culminating
+//
+//  Created by Lucas García on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipies_CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
