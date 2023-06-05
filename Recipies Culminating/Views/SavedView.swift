45383dc76a28b3f34c73b925980b81cd06e6dfd3
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-06-05 11:21:07 -0400

    I created the basic code for the view that shows the saved recipes

--- Recipies Culminating/Views/SavedView.swift
+++ Recipies Culminating/Views/SavedView.swift
@@ -8,10 +8,30 @@ import Blackbird
 import SwiftUI
 
 struct SavedView: View {
+    
+    @BlackbirdLiveModels({ db in
+        try await IngridientsLis.read(from: db)
+    }) var savedRecipes
+    
+    
     var body: some View {
-        VStack{
+       
+        NavigationView{
             
+            List(savedRecipes.results, id: \.id){currentAdvice in
+                VStack(alignment:.leading){
+                    Text(currentAdvice.name)
+                        .bold()
+                    Text("Steps")
+                        .bold()
+                    Text(currentAdvice.steps)
+                        
+                   
+                }
+            }
+            .navigationTitle("Your Recipes")
         }
+        
     }
 }
     struct SavedView_Previews: PreviewProvider {
